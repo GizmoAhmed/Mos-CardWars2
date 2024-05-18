28a402b740356fad692a2a78ae2fabbf6a06a0d8
GizmoAhmed <mohman1232@gmail.com>
2024-05-18 00:40:53 -0400

    landing zones

--- Card Wars 2_clone_0/Assets/Scripts/Card.cs
+++ Card Wars 2_clone_0/Assets/Scripts/Card.cs
@@ -8,22 +8,53 @@ public class Card : MonoBehaviour
     public bool Movable = true;
     public bool FaceUp; // true = visible, false = back
 
+    public GameObject GameCanvas;
+
+    public GameObject   DropZone;
+    public GameObject   StartParent;
+    public Vector2      StartPos;
+    public GameObject   NewDropZone;
+    public bool         isOverDropZone;
+
     void Start()
     {
         Movable = true;
+        GameCanvas = GameObject.Find("GameCanvas");
+		DropZone = GameObject.Find("DropZone");
+	}
+
+	public void Grab() 
+    { 
+        Grabbed = true;
+
+        // upon grab...
+        StartParent = transform.parent.gameObject;      // save parent 
+        StartPos = transform.position;                  // save position 
+
     }
 
-    public void Grab() { Grabbed = true; }
+    public void LetGo() 
+    {
+        Grabbed = false;
 
-    public void LetGo() { Grabbed = false; }
+        if (isOverDropZone)
+        {
+            transform.SetParent(NewDropZone.transform, false);
+        }
+        else 
+        {
+            transform.position = StartPos;
+            transform.SetParent(StartParent.transform, false);
+        }
+    }
 
-    // Update is called once per frame
     void Update()
     {
 
         if (Movable && Grabbed)
         {
             transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            transform.SetParent(GameCanvas.transform, true);
         }
     }
 
