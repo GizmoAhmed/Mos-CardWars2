fc0fd1ac876a3c24546a002474423b2b53f1cd3a
GizmoAhmed <mohman1232@gmail.com>
2024-07-25 23:03:02 -0400

    Just did a fullscreen playtest
    Spacing for Spells need some work. Considering simply making an active spell limit, as game design. Solutions will need to be explored.
    
    Spell Card isn't discarding correctly, via null ref. I know how to fix it, but not today:
    
    'The variable CurrentCard of SpellLand has not been assigned.'
    
    Spell land doesn't use current card, consider an override, added a comment, have gpt look into it

--- Card Wars 2/Assets/Scripts/Cards/Spacing.cs
+++ Card Wars 2/Assets/Scripts/Cards/Spacing.cs
@@ -5,23 +5,17 @@ using UnityEngine.UI;
 
 public class Spacing : MonoBehaviour
 {
-	public GridLayoutGroup grid;
-	public int cardCount;
+	private int cardCount;
 
 	[Header("ax^2 + bx + c")]
 	public float a = 2.66071f;
 	public float b = 54.9679f;
 	public float c = 183f;
 
-	void Start()
-	{
-		grid = GetComponent<GridLayoutGroup>();
-	}
-
 	void Update()
 	{
 		cardCount = transform.childCount;
-		grid.spacing = new Vector2(CalculateSpacing(cardCount), 0);
+		GetComponent<GridLayoutGroup>().spacing = new Vector2(CalculateSpacing(cardCount), 0);
 	}
 
 	float CalculateSpacing(int cardCount)
