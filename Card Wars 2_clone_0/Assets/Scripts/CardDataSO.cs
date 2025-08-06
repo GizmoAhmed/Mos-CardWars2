0219951a1ae71049b5ea36462c0e1ea640ac2c68
Mohamed Ahmed <ahmedm19@msu.edu>
2025-08-06 02:32:54 -0400

    new pc came in
    commiting so I can get the up to date stuff on there

---
+++ Card Wars 2_clone_0/Assets/Scripts/CardDataSO.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewCard", menuName = "Cards")]
+public class CardDataSO : ScriptableObject
+
+{
+    public String Name;
+    public enum CardType
+    {
+        Creature,
+        Building,
+        Spell,
+        Charm
+    }
+
+    public CardType cardType;
+
+    public Sprite MainImage;
+    public Sprite Element;
+
+    public int Attack, Defense, Magic;
+
+    public String description;
+    
+    // public CardEffectSO effect; 
+}
