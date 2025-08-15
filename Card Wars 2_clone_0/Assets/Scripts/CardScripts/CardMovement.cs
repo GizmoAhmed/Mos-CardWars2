7576e45857c070624a9840eea23a915d557bc676
Mohamed "Gizmo" Ahmed <mohman1232@gmail.com>
2025-08-14 23:22:41 -0400

    figured out where to put buildings

--- Card Wars 2_clone_0/Assets/Scripts/CardScripts/CardMovement.cs
+++ Card Wars 2_clone_0/Assets/Scripts/CardScripts/CardMovement.cs
@@ -1,18 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Mirror;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class CardMovement : MonoBehaviour
+public class CardMovement : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        Debug.Log("Card Movement Start for" + name);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerEnter()
     {
-        
+        Debug.Log("OnPointerEnter");
+    }
+
+    public void OnPointerExit()
+    {
+        Debug.Log("OnPointerExit");
+    }
+
+    public void BeginDrag()
+    {
+        Debug.Log("BeginDrag");
+    }
+
+    public void EndDrag()
+    {
+        Debug.Log("EndDrag");
     }
 }
