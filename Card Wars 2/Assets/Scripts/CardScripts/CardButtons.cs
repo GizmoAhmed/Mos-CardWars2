003877f2ab6a23a906c55dfe0f8416006a65d9f2
Mohamed "Gizmo" Ahmed <mohman1232@gmail.com>
2026-01-09 19:10:33 -0500

    simple burn

--- Card Wars 2/Assets/Scripts/CardScripts/CardButtons.cs
+++ Card Wars 2/Assets/Scripts/CardScripts/CardButtons.cs
@@ -1,17 +1,38 @@
+using System;
 using UnityEngine;
 
 namespace CardScripts
 {
     public class CardButtons : MonoBehaviour
     {
+        private CardStats cardStats;
+        private CardMovement cardMove;
+
+        private void Start()
+        {
+            cardStats = GetComponent<CardStats>();
+            cardMove = GetComponent<CardMovement>();
+        }
+
         public void Burn() 
         {
-            Debug.Log($"Burning: {gameObject.name}");
+            Debug.Log($"Burning... {gameObject.name}");
+
+            if (cardStats.thisCardOwner.money >= cardStats.burnCost) // enough money?
+            {
+                cardMove.Discard();
+                cardStats.thisCardOwner.money -= cardStats.burnCost;
+                Debug.Log($"Burned: {gameObject.name}");
+            }
+            else
+            {
+                Debug.LogWarning($"Not enough money to burn {gameObject.name}");
+            }
         }
 
         public void CreatureAbility()
         {
-            Debug.Log($"Creature Ability: {gameObject.name}");
+            Debug.Log($"Creature Ability-ing...{gameObject.name}");
         }
     }
 }
