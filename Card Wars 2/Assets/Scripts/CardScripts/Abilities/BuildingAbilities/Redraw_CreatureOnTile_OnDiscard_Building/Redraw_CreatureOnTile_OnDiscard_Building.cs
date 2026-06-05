814e8909f4e3d57fe719a08ecc5706b14491fc07
Mohamed "Gizmo" Ahmed <mohman1232@gmail.com>
2026-06-05 16:40:06 -0400

    redraw ability done
    this ability had me rethinking and redoing the entire draw card system over the past week. This marks the end of all of that.

--- Card Wars 2/Assets/Scripts/CardScripts/Abilities/BuildingAbilities/Redraw_CreatureOnTile_OnDiscard_Building/Redraw_CreatureOnTile_OnDiscard_Building.cs
+++ Card Wars 2/Assets/Scripts/CardScripts/Abilities/BuildingAbilities/Redraw_CreatureOnTile_OnDiscard_Building/Redraw_CreatureOnTile_OnDiscard_Building.cs
@@ -1,6 +1,10 @@
 using System.Linq;
 using AbilityEvents;
 using CardScripts.Abilities;
+using CardScripts.CardMovements;
+using CardScripts.CardStatss;
+using GameManagement;
+using PlayerStuff;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Redraw_CreatureOnTile_OnDiscard_Building", menuName = "Abilities/Building/Redraw_CreatureOnTile_OnDiscard_Building")]
@@ -9,10 +13,22 @@ public class Redraw_CreatureOnTile_OnDiscard_Building : PassiveAbilitySO
     public override void ExecuteAbility(GameObject thisCard, AbilityEventData eventData)
     {
         Debug.Log($"{thisCard.name} calls redraw on {eventData.target.gameObject.name}");
+
+        CreatureStats targetsCreatureStats = eventData.target.GetComponent<CreatureStats>();
+
+        string redrawID = targetsCreatureStats.cardData.cardID;
+
+        PlayerStats owningPlayer = eventData.target
+            .GetComponent<CreatureMovement>()
+            .thisCardOwnerPlayerStats;
         
+        MasterDeck masterDeck = FindObjectOfType<MasterDeck>();
+
+        // redraw creature
+        masterDeck.CreateThenSpawnCard(redrawID, owningPlayer);
         
-        
-        
+        // discard this building
+        thisCard.GetComponent<CardMovement>().ServerDiscard();
     }
     
     public void OnValidate()
