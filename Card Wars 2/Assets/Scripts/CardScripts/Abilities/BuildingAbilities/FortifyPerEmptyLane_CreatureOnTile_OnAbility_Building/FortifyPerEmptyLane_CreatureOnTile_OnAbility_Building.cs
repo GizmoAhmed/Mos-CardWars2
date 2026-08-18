725f73a20ad863a709e56b9f83e9ea1c9cb51f37
Mohamed "Gizmo" Ahmed <mohman1232@gmail.com>
2026-08-17 22:09:57 -0400

    empty lane reader ability
    per empty lane ability

---
+++ Card Wars 2/Assets/Scripts/CardScripts/Abilities/BuildingAbilities/FortifyPerEmptyLane_CreatureOnTile_OnAbility_Building/FortifyPerEmptyLane_CreatureOnTile_OnAbility_Building.cs
@@ -0,0 +1,27 @@
+using AbilityEvents;
+using CardScripts.CardStatss;
+using Extensions;
+using UnityEngine;
+
+namespace CardScripts.Abilities.BuildingAbilities.FortifyPerEmptyLane_CreatureOnTile_OnAbility_Building
+{
+    [CreateAssetMenu(fileName = "FortifyPerEmptyLane_CreatureOnTile_OnAbility_Building", menuName = "Abilities/Building/FortifyPerEmptyLane_CreatureOnTile_OnAbility_Building")]
+    public class FortifyPerEmptyLane_CreatureOnTile_OnAbility_Building : PassiveAbilitySO
+    {
+        public int defensePerEmptyLane;
+    
+        public override void ExecuteAbility(GameObject thisCard, AbilityEventData eventData)
+        {
+            GameObject creature = eventData.target.gameObject;
+        
+            CreatureStats creatureStats = creature.GetComponent<CreatureStats>();
+
+            int totalTiles = 4;
+            int activeCreatureCount = thisCard.Ext_GetAllActiveCreaturesForThisPlayer().Count;
+
+            int rate = totalTiles -  activeCreatureCount;
+            
+            creatureStats.ChangeCreatureDefense(defensePerEmptyLane * rate, buff:true);
+        }
+    }
+}
