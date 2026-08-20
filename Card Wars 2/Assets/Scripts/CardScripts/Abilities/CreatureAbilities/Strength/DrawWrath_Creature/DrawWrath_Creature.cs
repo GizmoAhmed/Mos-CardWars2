853103d1827c4fbb8d0f54bb67f9ba3f791e5e11
Mohamed "Gizmo" Ahmed <mohman1232@gmail.com>
2026-08-19 22:14:21 -0400

    simple draw card on ability
    ie got a creature to draw wrath right here

---
+++ Card Wars 2/Assets/Scripts/CardScripts/Abilities/CreatureAbilities/Strength/DrawWrath_Creature/DrawWrath_Creature.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using AbilityEvents;
+using CardScripts.Abilities;
+using CardScripts.CardData;
+using CardScripts.CardMovements;
+using GameManagement;
+using PlayerStuff;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DrawWrath_Creature", menuName = "Abilities/Creature/Strength/DrawWrath_Creature")]
+public class DrawWrath_Creature : ActiveAbilitySO
+{
+    public RuneDataSO wrath;
+    
+    public override void ExecuteAbility(GameObject thisCard, AbilityEventData eventData)
+    {
+        if (wrath == null)
+        {
+            Debug.LogError($"{name} on {thisCard.name} is missing <color=red>wrath</color>, and therefore can't spawn it. Aborting...");
+            return;
+        }
+
+        // Get owning player
+        PlayerStats owningPlayer = thisCard
+            .GetComponent<CardMovement>()
+            .thisCardOwnerPlayerStats;
+        
+        MasterDeck masterDeck = FindObjectOfType<MasterDeck>();
+        
+        masterDeck.CreateThenSpawnCard(wrath.cardID, owningPlayer);
+    }
+}
