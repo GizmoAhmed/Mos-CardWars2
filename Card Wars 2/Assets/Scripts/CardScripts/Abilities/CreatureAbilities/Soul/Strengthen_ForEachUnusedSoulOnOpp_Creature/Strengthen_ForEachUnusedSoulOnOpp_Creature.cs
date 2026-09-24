68bb1c610a0e5c50434e8721154d8cff560986ea
Mohamed "Gizmo" Ahmed <mohman1232@gmail.com>
2026-09-23 21:43:45 -0400

    opp soul reader, plus float rate
    float rate as in the rate we multiplying by per soul can be a float:
    
    int * float
    int str = (int)(oppCurrentSoul * strPerCurrentSoul); // floor

---
+++ Card Wars 2/Assets/Scripts/CardScripts/Abilities/CreatureAbilities/Soul/Strengthen_ForEachUnusedSoulOnOpp_Creature/Strengthen_ForEachUnusedSoulOnOpp_Creature.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using AbilityEvents;
+using CardScripts.Abilities;
+using CardScripts.CardStatss;
+using Extensions;
+using PlayerStuff;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Strengthen_ForEachUnusedSoulOnOpp_Creature", menuName = "Abilities/Creature/Soul/Strengthen_ForEachUnusedSoulOnOpp_Creature")]
+public class Strengthen_ForEachUnusedSoulOnOpp_Creature : ActiveAbilitySO
+{
+    public float strPerCurrentSoul;
+    
+    public override void ExecuteAbility(GameObject thisCard, AbilityEventData eventData)
+    {
+        // read op current soul
+        PlayerStats opp = thisCard.Ext_GetOwningPlayerStats().Ext_GetOpponentPlayerStats();
+        
+        int oppCurrentSoul = opp.currentSoul;
+        int str = (int)(oppCurrentSoul * strPerCurrentSoul); // floor
+        
+        // buff at rate
+        thisCard.GetComponent<CreatureStats>().UpdateCreatureStrength(amount: str, buff: true);
+    }
+}
